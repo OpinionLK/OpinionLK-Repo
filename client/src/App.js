9fab0348a9f0548bc948dd0d7d7ea6aa331e1cbf
NipunaDakshina <dakshinagn@gmail.com>
2023-08-12 10:07:23 +0530

    make dashboard layout

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CommunityManagerdashboard from "./pages/CommunityManager/dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <p>HELLO</p>
+      <CommunityManagerdashboard/>
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,13 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import { AuthContextProvider } from './context/AuthContext';
+import App from './App';
 
-import theme from './theme';
-import router from './routes/mainroutes';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import {
-  ChakraProvider,
-  extendTheme,
-
-} from '@chakra-ui/react'
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  // <StrictMode>
-  
-    <AuthContextProvider>
-      <ChakraProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </AuthContextProvider>
-  // {/* </StrictMode> */}
+  <App/>
 );
 
