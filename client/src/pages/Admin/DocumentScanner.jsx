a0a900ba05bbedb4727ef30f623ca4a4e89d5d29
Rashmi Gunawardana <2020is043@stu.ucsc.cmb.ac.lk>
2023-08-15 01:22:14 +0530

    added basic OCR

--- client/src/pages/Admin/DocumentScanner.jsx
+++ client/src/pages/Admin/DocumentScanner.jsx
@@ -1,43 +1,66 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import mindee from 'mindee'; // Import the Mindee library
 
 const DocumentScanner = () => {
   const [result, setResult] = useState('');
-  const apiKey = '5897c0feb71dba4ac54bff1192632f07'; // Replace with your Mindee API key
-  const apiUrl = 'https://api.mindee.net/v1/products/Rashmi99/drivers_liscense/v1/predict';
+  const apiKey = '5897c0feb71dba4ac54bff1192632f07'; // Replace with your actual Mindee API key
 
-  const handleUpload = async (e) => {
-    e.preventDefault();
-    const file = e.target.files[0];
+  const mindeeSubmit = (evt) => {
+    evt.preventDefault();
+    let myFileInput = document.getElementById('my-file-input');
+    let myFile = myFileInput.files[0];
 
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
+    if (!myFile) { return; }
 
-      try {
-        // Upload the file to Mindee API for processing
-        const response = await axios.post(apiUrl, formData, {
-          headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'multipart/form-data'
-          }
-        });
+    let data = new FormData();
+    data.append("document", myFile, myFile.name);
 
-        // Handle the response here
-        const responseData = response.data;
-        setResult(JSON.stringify(responseData, null, 2));
-      } catch (error) {
-        console.error('Error:', error);
-        setResult('An error occurred while processing the request.');
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        const response = JSON.parse(this.responseText);
+        const contentResults = getContentResults(response);
+        setResult(contentResults);
       }
+    });
+
+    xhr.open("POST", "https://api.mindee.net/v1/products/Rashmi99/drivers_liscense/v1/predict");
+    xhr.setRequestHeader("Authorization", `Token ${apiKey}`);
+    xhr.send(data);
+  };
+
+  const getContentResults = (response) => {
+    if (response && response.document && response.document.inference) {
+      const inference = response.document.inference;
+      const contentResults = inference.pages.flatMap(page =>
+        Object.values(page.prediction).flatMap(item => item.values)
+      ).map(value => value.content);
+
+      const formattedResults = {
+        "name": contentResults[7],
+        "address": contentResults.slice(0, 5).join(', '),
+        "date_of_birth": contentResults[5],
+        "blood_group": contentResults[6],
+        "nic": contentResults[9]
+      };
+
+      return JSON.stringify(formattedResults, null, 2);
     }
+    return 'No content results available.';
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    mindeeSubmit(e); // Call the mindeeSubmit function to handle the upload and API request
   };
 
   return (
     <div>
       <h1>Document Scanner</h1>
-      <input type="file" accept=".jpg, .jpeg, .png, .pdf" onChange={handleUpload} />
+      <form onSubmit={handleUpload}>
+        <input type="file" id="my-file-input" name="file" />
+        <input type="submit" />
+      </form>
       <div>
         <pre>{result}</pre>
       </div>
