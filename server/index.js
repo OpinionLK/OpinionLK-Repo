9b7f889b5727ba26fc3a130572a4d1def229d98d
Rashmi Gunawardana <2020is043@stu.ucsc.cmb.ac.lk>
2023-09-15 00:24:41 +0530

    added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/user.js';
 dotenv.config();
 
 const app = express();
-const ORIGIN_URL = process.env.ORIGIN_URL || 'http://localhost:3000';
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || '').split(',');
 const PORT = process.env.PORT || 3002;
 
 app.get('/', function (req, res) {
@@ -18,9 +18,21 @@ app.get('/', function (req, res) {
     message: 'Welcome to the OpinionLK API',
   });
 });
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true
+}
 // MIDDLEWARE
 app.use(express.json());
-app.use(cors({ origin: ORIGIN_URL, optionsSuccessStatus: 200 }));
+app.use(cors(corsOptions));
 
 // ROUTES
 app.use('/api/auth', authRoutes); 
