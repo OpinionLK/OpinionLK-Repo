cb36523622f176214784abb6223aa78ccda2e21e
Rashmi Gunawardana <2020is043@stu.ucsc.cmb.ac.lk>
2023-09-14 23:37:20 +0530

    add

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import authRoutes from './routes/auth.js';
 import clientRoutes from './routes/client.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swagger from 'swagger-ui-express';
 import cors from 'cors';
 import surveyRoutes from './routes/surveys.js';
 import userRoutes from './routes/user.js';
@@ -30,27 +28,6 @@ app.use('/api/client', clientRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/survey', surveyRoutes);
 app.use('/api/survey/images', express.static('./uploads/surveyheader'));
-
-// Swagger
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'OpinionLK API Documentation',
-      version: '0.0.1',
-      description: 'This is a simple CRUD API application made with Express and documented with Swagger',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3002',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'],
-};
-
-const specs = swaggerJSDoc(options);
-app.use('/api-docs', swagger.serve, swagger.setup(specs));
 
 mongoose
   .connect(process.env.MONGO_URL, {
