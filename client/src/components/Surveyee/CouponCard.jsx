1a234ce93ff5d923854872ea983042fd657cf32c
Anjali Rodrigo <anjalikrodrigo@gmail.com>
2023-09-30 10:31:47 +0530

    reward page prgress

--- client/src/components/Surveyee/CouponCard.jsx
+++ client/src/components/Surveyee/CouponCard.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
 
 import { 
-    Card, 
-    CardHeader, 
-    CardBody, 
-    CardFooter, 
     Text,
     Stack,
-    StackDivider,
     Box,
     Heading,
-    Divider,
-    ButtonGroup,
     Button,
     Image,
     Flex,
-    IconButton,
-    Input,
     Spacer,
     Progress,
-    extendTheme,
 } from '@chakra-ui/react'
 
+import CouponPopup from './CouponPopup'
 import CouponIMG from '../../images/couponimg.png'
 import { RiCopperCoinFill } from "react-icons/ri";
 
-const CouponCard = () => {
+const CouponCard = ({couponID, couponName, couponDescription, companyName, couponImage, couponPoints, userPoints}) => {
+
+    const percent = (userPoints/couponPoints)*100
 
     return (
         <Flex
@@ -42,33 +35,48 @@ const CouponCard = () => {
                 
                 />
             </Box>
-            <Box pl='5' width = '270px' height='auto'>
+            <Box pl='5' width = '250px' height='auto'>
                 <Stack mt='3' mb='4'>
-                    <Heading size='md'>Buy one get one free</Heading>
+                    <Heading size='md'>{couponName}</Heading>
                     <Text size = 'sm' color = '#A3AED0' mt='-2' mb='-1'>
-                        Bakes by Bella
+                        {companyName}
                     </Text>
                     <Text>
+                        {couponDescription}
                         Get another cupcake free with the purchase of any of our cupcakes on the standard menu.
                     </Text>
                 </Stack>
                 <Flex
                     position={'relative'}
-                    bottom='-7'
+                    bottom='1'
                     alignContent={'center'}
                     justifyContent={'center'}
                 >
-                    {/* <Text color='blue.600' fontSize='2xl'>
-                        $450
-                    </Text> */}
                     <Box pt='4' width='130px' pr='3'>
-                        <Progress value={50} width='100%' borderRadius='20px' colorScheme='purple' borderColor='#451CFC'/>
+                        <Progress value={percent} width='100%' borderRadius='20px' colorScheme='purple' borderColor='#451CFC'/>
                     </Box>
                     {/* <Progress value={50} width='50%' borderRadius='20px' color='#451CFC' /> */}
                     <Spacer/>
-                    <Button variant='solid' colorScheme='purple' leftIcon={<RiCopperCoinFill />}>
-                        400/600
-                    </Button>
+                    
+                    {userPoints >= couponPoints ? (
+                        // <Button variant='solid' colorScheme='purple' leftIcon={<RiCopperCoinFill />} bg='#583EFF'>
+                        //     {couponPoints}
+                        // </Button>
+                        <CouponPopup 
+                            couponID={couponID}
+                            couponName={couponName}
+                            couponDescription={couponDescription}
+                            companyName={companyName}
+                            couponImage={couponImage}
+                            couponPoints={couponPoints}
+                            userPoints={userPoints}
+                        />
+                    ) : (
+                        /* Another type of button */
+                        <Button variant='solid' colorScheme='gray' leftIcon={<RiCopperCoinFill />}>
+                        {userPoints}/{couponPoints}
+                        </Button>
+                    )}
                 </Flex>
             </Box>
         </Flex>
