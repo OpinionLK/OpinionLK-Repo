8726b57fba50fafbb632deced3180a1671321e91
Rashmi Gunawardana <2020is043@stu.ucsc.cmb.ac.lk>
2023-11-01 13:48:13 +0530

    added

--- server/controllers/AddMemberC.js
+++ server/controllers/AddMemberC.js
@@ -1,6 +1,5 @@
 import ComManager from "../models/ComManagerModel.js";
 import nodemailer from 'nodemailer';
-import { Clients } from '../models/Client.js';
 
 export const Getmembers = async (req, res) => {
      // #swagger.tags = ['Community Manager']
@@ -10,7 +9,6 @@ export const Getmembers = async (req, res) => {
 };
 
 export const Savemember = async (req, res) => {
-  // console.log(req.body);
   const member = {
     ManagerFirstName: req.body.ManagerFirstName,
     ManagerLastName: req.body.ManagerLastName,
@@ -22,7 +20,7 @@ export const Savemember = async (req, res) => {
     ManagerNic: req.body.ManagerNic,
   }
 
-  //email part 
+  // Create the nodemailer transporter
   const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
@@ -33,47 +31,55 @@ export const Savemember = async (req, res) => {
       pass: process.env.PASSWORD,
     },
     tls: {
-        rejectUnauthorized: false
-      }
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
-    from:{
-        name: 'Opinion.lk',
-        address: process.env.USER
+    from: {
+      name: 'Opinion.lk',
+      address: process.env.USER
     },
-    to: req.body.ManagerEmail, 
+    to: req.body.ManagerEmail,
     subject: "Welcome to OpinionLK",
     text: `Hello ${req.body.ManagerFirstName} ${req.body.ManagerLastName},\n\nWelcome to OpinionLK! We're thrilled to have you on board. It's time to connect with us and join the OpinionLK community.`,
     html: `<p>Hello ${req.body.ManagerFirstName} ${req.body.ManagerLastName},</p><p>Welcome to OpinionLK! We're thrilled to have you on board. It's time to <a href='https://opinionlk.me'>log into your account </a> and join the OpinionLK community.</p>`,
-    }
-
-    const sendMail = async (transporter, mailOptions) => {
-      try {
-          const info = await transporter.sendMail(mailOptions);
-          console.log(info);
-          return res.status(200).json({"message": "Email sent successfully"});
-      } catch (error) {
-          console.log(error);
-          return res.status(500).json({"message": "Email sent failed"});
-      }
   }
-  
-    let user = await ComManager.findOne({ ManagerEmail: req.body.ManagerEmail });
+
+  // Define the sendMail function
+  const sendMail = async (transporter, mailOptions) => {
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log(info);
+      return {"message": "Email sent successfully"};
+    } catch (error) {
+      console.log(error);
+      return {"message": "Email sent failed"};
+    }
+  }
+
+  // Check if the user with the same email already exists in ComManager
+  let user = await ComManager.findOne({ ManagerEmail: req.body.ManagerEmail });
 
   if (user) {
     return res.status(400).json({ error: 'User already exists' });
   }
 
-  await ComManager.create( member)
+  // Create a new ComManager entry
+  ComManager.create(member)
     .then((data) => {
-      sendMail(transporter,mailOptions);
-      console.log("Saved Successfully...");
-      res.status(201).send(data);
+      const emailResult = sendMail(transporter, mailOptions);
+
+      if (emailResult.message === "Email sent successfully") {
+        console.log("Saved Successfully...");
+        res.status(201).send(data);
+      } else {
+        res.status(500).json({ error: "Failed to send confirmation email" });
+      }
     })
     .catch((err) => {
       console.log(err);
-      res.send({ error: err, msg: "Something went wrong!" });
+      res.status(500).send({ error: err, msg: "Something went wrong!" });
     });
 };
 
@@ -109,7 +115,6 @@ export const Updatemember = async (req, res) => {
     res.status(500).json({ error: "Something went wrong!" });
   }
 };
-
 
 export const Deletemember = (req, res) => {
   const { id } = req.params;
