aabe3b0d8626db9b5ef888cf16bd87618726c590
NipunaDakshina <87487225+NipunaDakshina@users.noreply.github.com>
2023-11-01 23:10:26 +0530

    add overall survey static

---
+++ client/src/components/CommunityManager/StaticsComponents/OverrallStat.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { 
+    Card, 
+    Box,
+} from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import {Line,} from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+
+// sample data
+const state = {
+    labels: ['January', 'February', 'March',
+             'April', 'May'],
+    datasets: [
+      {
+        label: 'Surveys',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: '#fff',
+        borderColor: '#4318FF',
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
+  }
+
+const OverallStat = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios.get('/getCountWithStatus') // Assuming this is the route on your Express backend
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data: ', error);
+        });
+    }, []);
+
+
+
+
+
+    return (
+
+        <Card
+            size='md'
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='elevated'
+            // p={[2, 3]}
+            mt={5}
+            borderRadius='20'
+            align='center'
+        >
+            <Box
+                position="relative"
+                width="570px"  // Set your desired width here
+                height="300px" // Set your desired height here
+                overflow="hidden"
+                p={[2, 3]}
+            >
+                <Line
+                    data={state}
+                    options={{
+                        title:{
+                            display:true,
+                            text:'Average Rainfall per month',
+                            fontSize:20
+                        },
+                        legend:{
+                            display:true,
+                            position:'right'
+                        }
+                    }}
+                />
+            </Box>
+        </Card>
+
+    )
+}
+
+export default OverallStat;
--- client/src/components/CommunityManager/ViewAllSurveys.jsx
+++ client/src/components/CommunityManager/ViewAllSurveys.jsx
@@ -14,14 +14,20 @@ import {
   Heading,
   InputGroup,
   InputLeftElement,
+  Card,
 } from "@chakra-ui/react";
 
 import {
-    SearchIcon
+  SearchIcon
 } from '@chakra-ui/icons';
 
 
 import Pagination from "./Pagination";
+
+function formatDate(dateString) {
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  return new Date(dateString).toLocaleDateString('en-US', options);
+}
 
 function AllSurveyList() {
   const [surveys, setSurveys] = useState([]);
@@ -68,41 +74,44 @@ function AllSurveyList() {
   return (
     <div>
 
-<Flex mb={'20px'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
+      <Flex mb={'20px'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
 
-<Heading size={'md'}>
-    List of Surveys
-</Heading>
+        <Heading size={'md'}>
+          List of Surveys
+        </Heading>
 
-<InputGroup width={'40%'}>
-    <InputLeftElement
-        pointerEvents="none"
-        children={<SearchIcon color="gray.300" />}
-    />
-    <Input type="text" placeholder="Search" onChange={handleSearch} value={searchTerm} />
-</InputGroup>
-</Flex>
-
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Description</Th>
-            <Th>Approval Status</Th>
-            <Th>Created Date</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {currentSurveys.map((survey) => (
-            <Tr key={survey.surveyId}>
-              <Td>{survey.surveyName}</Td>
-              <Td>{survey.surveyDescription}</Td>
-              <Td>{survey.approvalStatus}</Td>
-              <Td>{survey.created_date}</Td>
+        <InputGroup width={'40%'}>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input type="text" placeholder="Search" onChange={handleSearch} value={searchTerm} />
+        </InputGroup>
+      </Flex>
+      <Card align={"center"} pt={5} borderRadius={10}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Description</Th>
+              <Th>Question Count</Th>
+              <Th>Approval Status</Th>
+              <Th>Created Date</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {currentSurveys.map((survey) => (
+              <Tr key={survey.surveyId}>
+                <Td>{survey.surveyName}</Td>
+                <Td>{survey.surveyDescription}</Td>
+                <Td>{survey.questions.length}</Td>
+                <Td>{survey.approvalStatus}</Td>
+                <Td>{formatDate(survey.created_date)}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Card>
       <Center pt={10} pb={10}>
         <Pagination
           currentPage={currentPage}
